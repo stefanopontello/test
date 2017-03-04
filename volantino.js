9a1e5a83d9c82c286804e667a51b44446eb81a2c
Stefano Pontello <Stefano Pontello>
2017-03-04 18:55:21 +0100

    Update to JSON

---
+++ volantino.js
@@ -0,0 +1,22 @@
+var myData = { 
+    "001" : { 
+	    		"description" : "Purchased 404 worth of furniture and paid in account.", 
+	    		"debitname": "Furniture",
+				"creditname": "Accounts payable"
+			},
+    "002" : { 
+	    		"description" : "Received 8745 of cash due to a service provided", 
+	    		"debitname": "Cash",
+				"creditname": "Accounts receivable"
+    		 },
+    "003" : { 
+	    		"description" : "Paid 8227 with cash for the insurance contract", 
+	    		"debitname": "Insurace expenses",
+				"creditname": "Check"
+    		 },
+    "004" : { 
+	    		"description" : "An amount of $50,000 was paid for six months of rent.", 
+	    		"debitname": "Prepaid Rent",
+				"creditname": "Cash"
+    		 }
+}
